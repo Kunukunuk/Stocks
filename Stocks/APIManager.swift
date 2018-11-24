9ffe6d0d0545b80d7d34a04f3195a8febd57718d
Kun Huang <kkun.huang@gmail.com>
2018-11-23 22:04:32 -0500

    getting stock by symbol

--- Stocks/APIManager.swift
+++ Stocks/APIManager.swift
@@ -11,12 +11,25 @@ import Foundation
 class APIManager {
     
     var session: URLSession
+    let basicURL = "https://api.iextrading.com/1.0"
     
     init() {
         session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
     }
     
     func getStockData() {
+        
+        let apiURL = URL(string: basicURL + "/stock/market/batch?symbols=aapl,fb,tsla&types=quote,news,chart&range=1m&last=5")
+        let task = URLSession.shared.dataTask(with: apiURL!) { (data, response, error) in
+            guard let dataJson = data else {
+                print(error?.localizedDescription)
+                return
+            }
+            let dataDictionary = try! JSONSerialization.jsonObject(with: dataJson, options: []) as! [String: Any]
+            
+            print(dataDictionary)
+        }
+        task.resume()
         
     }
 }
